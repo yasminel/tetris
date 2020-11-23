ddd65c6242b9861256935fabff2e75d8cbb8fb4d
yasminel <66360379+yasminel@users.noreply.github.com>
2020-11-23 21:10:44 +0100

    draw grid and set constants

---
+++ Tetris/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace Tetris
+{
+    class Config
+    {
+        public const int ROWS = 20;
+        public const int COLUMNS = 10;
+        public const int SQUARE_SIZE = 25;
+        public const int WINDOW_WIDTH = COLUMNS * SQUARE_SIZE;
+        public const int WINDOW_HEIGHT = ROWS * SQUARE_SIZE;
+    }
+}
--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -3,22 +3,25 @@ using Timers = System.Timers;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Timers;
+using System.Drawing.Drawing2D;
 
 namespace Tetris
 {
     public partial class Form1 : Form
     {
         private Timers.Timer timer;
-        private int WIDTH = 800;
-        private int HEIGHT = 600;
-        private float FPS = 120f;
-        private float FRAMESIZE = 1000f / 120f;
+        private const float FPS = 120f;
+        private float FRAMESIZE = 1000f / FPS;
 
         public Form1()
         {
             InitializeComponent();
             DoubleBuffered = true;
-            Size = new Size(WIDTH, HEIGHT);
+            ClientSize = new Size(Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Text = "Tetris";
             Load += OnLoad;
         }
 
@@ -42,7 +45,23 @@ namespace Tetris
 
         protected override void OnPaint(PaintEventArgs e)
         {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
 
+            g.Clear(Color.FromArgb(33, 33, 33));
+
+            for (int i = 0; i < Config.COLUMNS; i++)
+            {
+                g.DrawLine(new Pen(Color.Gray), i * Config.SQUARE_SIZE, 0, i * Config.SQUARE_SIZE, Config.WINDOW_HEIGHT);
+            }
+            for (int i = 0; i < Config.ROWS; i++)
+            {
+                g.DrawLine(new Pen(Color.Gray), 0, i * Config.SQUARE_SIZE, Config.WINDOW_WIDTH, i * Config.SQUARE_SIZE);
+            }
+
+            // draw player
+
+            // draw opponent
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
